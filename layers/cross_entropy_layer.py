0f48bd65d9565c6c3498df847fe0bae61980978f
Mohamed Haseeb <mohamedaziz52@gmail.com>
2016-06-24 16:27:48 +0200

    cross entropy done

--- layers/cross_entropy_layer.py
+++ layers/cross_entropy_layer.py
@@ -1,22 +1,33 @@
+from __future__ import print_function
+from __future__ import division
 import numpy as np
 
 
 class CrossEntropyLayer:
-    def __init__(self, input_size):
-        self.input_size = input_size
+    def __init__(self):
         self.output_size = 1
         # layer input holder
-        self.current_input = None
+        self.current_input_probabilities = None
         # layer output holder
         self.current_output = None
         # derivative of Loss w.r.t. inputs
         self.dL_dinput = None
+        # target probabilities
+        self.current_target_probabilities = None
 
-    def forward(self, layer_input, targets):
-        self.current_input = layer_input
+    def set_current_target_probabilities(self, target_probabilities):
+        self.current_target_probabilities = target_probabilities
+
+    def forward(self, layer_input):
+        self.current_input_probabilities = layer_input
         self.current_output = np.sum(
-            -1 * targets * np.log(self.current_input) + (targets - 1) * np.log(1 - self.current_input))
+            -1 * self.current_target_probabilities * np.log(self.current_input_probabilities) + (
+                self.current_target_probabilities - 1) * np.log(
+                1 - self.current_input_probabilities))
         return self.current_output
 
     def backward(self, dL_dout):
-        pass
+        self.dL_dinput = -self.current_target_probabilities / self.current_input_probabilities + \
+                         (1 - self.current_target_probabilities) / (1 - self.current_input_probabilities)
+        self.dL_dinput *= dL_dout
+        return self.dL_dinput
--- test/test_cross_entropy_layer.py
+++ test/test_cross_entropy_layer.py
@@ -1,16 +1,38 @@
 import numpy as np
 from layers.cross_entropy_layer import CrossEntropyLayer
+from util import utils
 
 
 def test_forward():
-    n_inputs = 5
     # create a cross entropy layer
-    layer = CrossEntropyLayer(n_inputs)
+    layer = CrossEntropyLayer()
     # create a layer input,
-    layer_input = np.array([0.1, 0.4, 0.5])
-    target = np.array([0, 0, 1])
+    input_probabilities = np.array([0.1, 0.4, 0.5])
+    target_probabilities = np.array([0, 0, 1])
     # execute the layer
-    layer_output = layer.forward(layer_input, target)
+    layer.set_current_target_probabilities(target_probabilities)
+    layer_output = layer.forward(input_probabilities)
     # compare the layer output to the expected one
     output_truth = 1.30933331998  # calculated by hand
     assert (layer_output, output_truth)
+
+
+def test_backward():
+    n_inputs = 4
+    # create a layer
+    layer = CrossEntropyLayer()
+    # create a layer input,
+    input_probabilities = np.array([[0.7, 0.4, 0.5, 0.1]])
+    target_probabilities = np.array([[0, 1, 0, 0]])
+    # create dL_layer_doutput
+    dL_doutput = 1
+    # perform a forward and a backward pass
+    layer.set_current_target_probabilities(target_probabilities)
+    layer.forward(input_probabilities)
+    dL_dinput = layer.backward(dL_doutput)
+    # verify dL_dinput ######
+    doutput_dinput_truth = utils.approximate_derivative(layer.forward, input_probabilities, 1,
+                                                        h=0.000001)  # n_outputs X n_inputs
+    dL_dinput_truth = np.dot(dL_doutput, doutput_dinput_truth)
+    result = np.isclose(dL_dinput, dL_dinput_truth)
+    assert result.all()
