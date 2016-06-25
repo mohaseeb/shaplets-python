1a9990f770e7b0a1efd1bc48dc552f13cddd03ba
Mohamed Haseeb <mohamedaziz52@gmail.com>
2016-06-25 13:59:25 +0200

    updating params for linear layer

--- layers/linear_layer.py
+++ layers/linear_layer.py
@@ -5,7 +5,7 @@ import numpy as np
 
 
 class LinearLayer:
-    def __init__(self, input_size, output_size):
+    def __init__(self, input_size, output_size, learning_rate, lamda, training_size):
         """
 
         :param input_size:
@@ -13,12 +13,18 @@ class LinearLayer:
         """
         self.input_size = input_size
         self.output_size = output_size
+        # learning rate
+        self.eta = learning_rate
+        # regularization factor
+        self.lamda = lamda
+        # training data size
+        self.I = training_size
         # layer weights
         self.W = None
         # layer biases
         self.W_0 = None
-        self.set_weights(np.random.normal(loc=0, scale=1, size=(output_size, input_size)),
-                         np.random.normal(loc=0, scale=1, size=(output_size, 1)))
+        self.set_weights(np.random.normal(loc=0, scale=1, size=(1, output_size * input_size)),
+                         np.random.normal(loc=0, scale=1, size=(1, output_size)))
         # layer input holder
         self.current_input = None
         # layer output holder
@@ -39,7 +45,8 @@ class LinearLayer:
         :return:
         """
         self.current_input = layer_input
-        self.current_output = np.dot(self.W, self.current_input.T) + self.W_0
+        self.current_output = np.dot(np.reshape(self.W, (self.output_size, self.input_size)),
+                                     self.current_input.T) + np.reshape(self.W_0, (self.output_size, 1))
         return self.current_output.T
 
     def backward(self, dL_dout):
@@ -59,16 +66,18 @@ class LinearLayer:
             output_W_0_index += 1
         self.dL_dparams = np.dot(dL_dout, dout_dparams)
         # dL_dinputs calculations
-        self.dL_dinput = np.dot(dL_dout, self.W)
+        self.dL_dinput = np.dot(dL_dout, np.reshape(self.W, (self.output_size, self.input_size)))
         return self.dL_dinput, self.dL_dparams
 
-    def update_params(self, update_matrix):
+    def update_params(self):
         """
 
         :param update_matrix:
         :return:
         """
-        pass
+        self.W += - self.eta * (
+            self.dL_dparams[0, 0:self.W.size] + 2 * self.lamda * self.W / (self.I * self.output_size))
+        self.W_0 += - self.eta * self.dL_dparams[0, self.W.size:]
 
     def get_params(self):
         """
--- test/test_linear_layer.py
+++ test/test_linear_layer.py
@@ -8,7 +8,10 @@ from util import utils
 def test_fc_layer_initialization():
     n_inputs = 15
     n_outputs = 4
-    fc_layer = LinearLayer(n_inputs, n_outputs)
+    learning_rate = 0.01
+    regularization_parameter = 0.1
+    training_size = 10
+    fc_layer = LinearLayer(n_inputs, n_outputs, learning_rate, regularization_parameter, training_size)
     W = fc_layer.get_params()
     assert (W.shape == (1, n_outputs * (n_inputs + 1)))
 
@@ -16,8 +19,10 @@ def test_fc_layer_initialization():
 def test_forward():
     n_inputs = 15
     n_outputs = 4
-    # create a layer
-    fc_layer = LinearLayer(n_inputs, n_outputs)
+    learning_rate = 0.01
+    regularization_parameter = 0.1
+    training_size = 10
+    fc_layer = LinearLayer(n_inputs, n_outputs, learning_rate, regularization_parameter, training_size)
     # initialize weights
     W = np.ones((n_outputs, n_inputs))
     W_0 = np.ones((n_outputs, 1))
@@ -38,8 +43,10 @@ def test_backword():
     # create a layer
     n_inputs = 3
     n_outputs = 2
-    # create a layer
-    fc_layer = LinearLayer(n_inputs, n_outputs)
+    learning_rate = 0.01
+    regularization_parameter = 0.1
+    training_size = 10
+    fc_layer = LinearLayer(n_inputs, n_outputs, learning_rate, regularization_parameter, training_size)
     # create a layer input
     layer_input = np.random.normal(loc=0, scale=1, size=(1, n_inputs))
     # create dL_layer_doutput
@@ -56,7 +63,5 @@ def test_backword():
     # verify dL_dW
     dout_dparams_truth = utils.approximate_derivative_wrt_params(fc_layer, layer_input, n_outputs, h=0.0001)
     dL_dparams_truth = np.dot(dL_layer_output, dout_dparams_truth)
-    print(dL_dparams)
-    print(dL_dparams_truth)
     result = np.isclose(dL_dparams, dL_dparams_truth, rtol=1e-05, atol=1e-03)
     assert result.all()
