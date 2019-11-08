9a7efd23f19e447dc4ed794c7ca469fd983172da
Mohamed Haseeb <m@mohaseeb.com>
2019-11-09 00:29:17 +0100

    v0.3.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='shapelets-lts',
-    version='0.3.0',
+    version='0.3.1',
     install_requires=[
         'numpy>=1.15.4,<2.0.0',
         'pandas>=0.23.4,<2.0.0'
